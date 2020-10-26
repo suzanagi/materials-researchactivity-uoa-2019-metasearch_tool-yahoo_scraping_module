31605fb51bdace711ad22a31c28a14fcac31364c
suzanagi <realyutanemoto@gmail.com>
2020-10-27 01:53:35 +0900

    fixed yahoo search module to include snippet text into ResultItems correctly

--- yahoo_search_module.py
+++ yahoo_search_module.py
@@ -3,9 +3,12 @@ import sys
 from bs4 import BeautifulSoup
 from result_item import ResultItem
 
-def search(query):
+def retrieve_result_page(query: str):
+    return requests.get('https://search.yahoo.com/search?p='+query)
+
+def search(query: str):
     # Get the Yahoo search result page for the query
-    page = requests.get('https://search.yahoo.com/search?p='+query)
+    page = retrieve_result_page(query)
     # Prepare a list for returning the search results
     result = list()
     # Check the result page encoding to use it in BeautifulSoup composition
@@ -13,15 +16,34 @@ def search(query):
     # Analyse the result page using BeautifulSoup
     soup = BeautifulSoup(page.content, "html.parser", from_encoding = encoding)
     # Obtain topics and abstract element by the BeautifulSoup function
-    topics = soup.find_all("a", attrs={"ac-algo", "fz-l"})
-    abstract = soup.find_all("p", attrs={"lh-16"})
     # Put the results in the list to be returned
     rank = 1
-    for title in topics:
-        r_item = ResultItem(title.text, title.attrs['href'], "Yahoo!")
-        r_item.add_rank(rank)
+    result_items = soup.select("div.dd.algo.algo-sr.relsrch")
+    for item in result_items:
+        # Retrieve title and URL
+        title_section = item.find("h3", attrs={"title"})
+        title_section = title_section.find("a", attrs={"ac-algo", "fz-l", "ac-21th", "lh-24"})
+        title: str = title_section.text
+        url: str = title_section.attrs['href']
+        snippet: str = ""
+        # Retrieve snippets
+        snippet_section = item.find("div", attrs={"compText", "aAbs"})
+        if snippet_section == None:
+            snippet_section = item.find("ul", attrs={"pl-15"})
+            if snippet_section == None:
+                snippet = ""
+            else:
+                snippet = snippet_section.text
+        else:
+            snippet: str = snippet_section.find("p", attrs={"fz-ms", "lh-1_43x"}).text
+            
+        
+        r_item: ResultItem = ResultItem(title, url, "Yahoo!")
+        r_item.set_rank(rank)
+        r_item.set_abstract(snippet)
+        rank = rank + 1
         result.append(r_item)
-        rank += 1
+
     # Return the result list
     return result
 
@@ -36,7 +58,6 @@ if __name__ == "__main__":
     result = search(query)
     # Print the result list to the command line
     for item in result:
-        print("[title] "+item.title)
-        print("[url] "+item.url)
-        print("[rank] "+str(item.rank))
-        print("\n")
+        print(item)
+        print(item.get_abstract())
+        
